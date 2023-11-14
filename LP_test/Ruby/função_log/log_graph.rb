670a74136172193fe5137eb3e37d257075e687a0
Victor Guilherme <almirante_ming@ctOS3.0>
2023-11-14 17:17:53 -0400

    atualização do logarítmo, com ascii

--- LP_test/Ruby/função_log/log_graph.rb
+++ LP_test/Ruby/função_log/log_graph.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+require 'ascii_charts.rb'
+
 puts "digite o valor do logaritimando: "
 val = gets.chomp.to_f
 
@@ -5,9 +8,12 @@ puts "Digite o valor da base: "
 bas = gets.chomp.to_f
 
 if val > 0 && val != 1;
+
   res = Math.log(val, bas)
-  puts res
+  puts "o logarítmo de #{val}, na base #{bas}, é:  #{res}"
 
 else
-  puts "digite valores válidos!"
+  puts "digite valores válidos !"
 end
+
+puts AsciiCharts::Cartesian.new([[0,1],[1,3],[2,7],[2.5,12],[3,15]]).draw
