4abeda14ef9f9b003b7b109ce52d33820bdcdc9f
Victor Guilherme <almirante_ming@ctOS3.0>
2023-11-29 17:29:09 -0400

    math.log in ruby update

--- LP_test/Ruby/função_log/calc_pitch.rb
+++ LP_test/Ruby/função_log/calc_pitch.rb
@@ -1,6 +1,6 @@
 #! /etc/bin/env ruby
 
-def calc_pitch(f, f0=1, p0=1)
+def calc_pitch(frequency, refer_frequency=1, refer_pitch=1)
   p=p0+12*Math.log2(f/f0)
   return p
 end
--- LP_test/Ruby/função_log/core_AI.rb
+++ LP_test/Ruby/função_log/core_AI.rb
@@ -1,9 +1,20 @@
-require 'asciicharts'
+require 'ascii_charts'
+import 'calc_pitch'
 
-data = [[1, 2], [2, 3], [3, 1], [4, 2], [5, 3]]
+# coleta as variáveis
+puts "Digite a frequência desejada: "
+f = gets.chomp.to_f
+puts "digite o valor da frequencia de referência (f⁰), ou pressione enter para usar o padrão: "
+f0 = gets.chomp.to_f
+puts "Digite o valor do tom de referência (p⁰), ou pressione enter para usar o valor padrão: "
+p0 = gets.chomp.to_f
 
-# Create a Cartesian chart with the data
-chart = AsciiCharts::Cartesian.new(data, type: :scatter)
+#cria a variável "graph e define uma função para ela"
+graph = calc_pitch(f, f0, p0)
 
-# Draw the chart
+#define um array com os valores que serão usados no gráfico
+data = (0...10).to_a.map { |x| [x, graph] }
+
+#cria o gráfico
+chart = AsciiCharts::Cartesian.new(data, title: 'y = e^x')
 puts chart.draw
