4f9cb296777cd39fb00da01baf75d876374b08e6
V. Guilherme <132358726+Almirante-Ming@users.noreply.github.com>
2023-11-29 22:00:41 -0400

    wsl

--- LP_test/Ruby/função_log/calc_pitch.rb
+++ LP_test/Ruby/função_log/calc_pitch.rb
@@ -2,4 +2,4 @@
 def calc_pitch(frequency, refer_frequency=1, refer_pitch=1)
   p=p0+12*Math.log2(f/f0)
   return p
-ends
+end
---
+++ LP_test/Ruby/função_log/gnuplot_tst.rb
@@ -0,0 +1,33 @@
+require 'gnuplot'
+
+def calc_pitch(f, f0=1, p0=1)
+ pitch = p0 + 12 * Math.log2(f / f0)
+end
+
+puts "Digite a frequência desejada: "
+f = gets.chomp.to_f
+puts "digite o valor da frequencia de referência (f⁰), ou pressione enter para usar o padrão: "
+f0 = gets.chomp.to_f
+puts "Digite o valor do tom de referência (p⁰), ou pressione enter para usar o valor padrão: "
+p0 = gets.chomp.to_f 
+
+# Frequency values for x-axis
+x = (200..1000).step(50).to_a
+
+# Function values for y-axis
+y = x.map { |f| calc_pitch(f, f0=1, p0=1) }
+
+Gnuplot.open do |gp|
+ Gnuplot::Plot.new(gp) do |plot|
+    plot.xrange "[200:1000]"
+    plot.yrange "[41:108]"
+    plot.title "Pitch per Frequency"
+    plot.xlabel "Frequency (Hz)"
+    plot.ylabel "Pitch (s)"
+
+    plot.data << Gnuplot::DataSet.new([x, y]) do |ds|
+      ds.with "lines"
+      ds.notitle
+    end
+ end
+end
