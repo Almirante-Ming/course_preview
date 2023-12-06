16a41ef62257b49497123e2159ad3c540fd4e948
Victor Guilherme <almirante_ming@ctOS3.0>
2023-12-06 15:59:40 -0400

    pré-prova

--- LP_test/Ruby/função_log/gnuplot_tst.rb
+++ LP_test/Ruby/função_log/gnuplot_tst.rb
@@ -1,21 +1,23 @@
 require 'gnuplot'
 
-def calc_pitch(f, f0=1, p0=1)
+#calcula a oitava usando uma notade referência
+def calc_pitch(f, f0=1, p0=0)
  pitch = p0 + 12 * Math.log2(f / f0)
 end
 
+# coleta os dados nescessários
 puts "Digite a frequência desejada: "
 f = gets.chomp.to_f
-puts "digite o valor da frequencia de referência (f⁰), ou pressione enter para usar o padrão: "
+puts "digite o valor da frequencia de referência (f⁰), ou pressione enter para usar o valor padrão: "
 f0 = gets.chomp.to_f
 puts "Digite o valor do tom de referência (p⁰), ou pressione enter para usar o valor padrão: "
-p0 = gets.chomp.to_f 
+p0 = gets.chomp.to_f
 
 # Frequency values for x-axis
 x = (200..1000).step(50).to_a
 
 # Function values for y-axis
-y = x.map { |f| calc_pitch(f, f0=1, p0=1) }
+y = x.map { |f| calc_pitch(f, f0, p0) }
 
 Gnuplot.open do |gp|
  Gnuplot::Plot.new(gp) do |plot|
--- LP_test/Ruby/função_log/log.py
+++ LP_test/Ruby/função_log/log.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
+# define a função a ser calculada junto aos parâmetros
 def calcular_pitch(f, f0=1, p0=0):
     p=p0+12*np.log2(f/f0)
     return p
+
+#coleta de dados
+f = float(input("Digite a frequência desejada: "))
+f0 = float(input("digite o valor da frequencia de referência (f⁰), ou pressione enter para usar o valor padrão: "))
+p0 = float(input("Digite o valor do tom de referência (p⁰), ou pressione enter para usar o valor padrão: "))
 
 # define o período do gráfico
 tempos_grafico = np.linspace(0.1, 10, 100)
